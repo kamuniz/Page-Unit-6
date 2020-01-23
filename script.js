90cb94c11ceeb6f096c15ad833a5e9a79ab1a599
kamuniz <60242373+kamuniz@users.noreply.github.com>
2020-01-23 14:43:17 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$("button").click(function() {
+    $("img").attr("src", "https://i.ytimg.com/vi/QjNO3T9YgxA/maxresdefault.jpg");
+
+});
+$("img").dblclick(function() {
+            $("body").append('<img src="https://cdn.kiwilimon.com/recetaimagen/34016/39654.jpg">');
+            });
+            $("h1").hover(function(){
+                $("h1").text("Tacos Finos");
+            });
+            
